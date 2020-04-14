1b07af250bea2f58cfd7f4721f23524521dbdcf7
Daniel Wallis <dwallis@dwallis-macbookpro.roam.corp.google.com>
2020-04-14 11:04:06 -0400

    More refactoring

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 //@ts-check
 (function() {
+    /**
+     * @template T
+     * @param {T[]} arr 
+     * @param {T} item 
+     */
+    function addIfDefined(arr, item) {
+        if (item) {
+            arr.push(item);
+        }
+    }
+
     class Cell {
         /**
          * @param {boolean} isAlive 
@@ -18,66 +29,63 @@
             this.board = board;
             this.isPaused = false;
         }
-    }
 
-    /**
-     * 
-     * @param {Cell} cell 
-     * @param {Cell[]} neighbours 
-     */
-    function getNewState(cell, neighbours) {
-        let numAliveNeighbours = neighbours.filter(n => n.isAlive).length;
-        if (cell.isAlive) {
-            return numAliveNeighbours == 2 || numAliveNeighbours == 3;
-        } else {
-            return numAliveNeighbours == 3;
-        }
-    }
-
-    /**
-     * 
-     * @param {Cell[]} neighbours 
-     * @param {Cell} cell 
-     */
-    function addIfDefined(neighbours, cell) {
-        if (cell) {
-            neighbours.push(cell);
-        }
-    }
-
-    /**
-     * @param {Cell[][]} board
-     */
-    function evaluateBoard(board) {
-        for (let rowIndex = 0; rowIndex < board.length; rowIndex++) {
-            for (let columnIndex = 0; columnIndex < board[rowIndex].length; columnIndex++) {
-                const neighbours = [];
-                const cell = board[rowIndex][columnIndex];
-                const previousRow = board[rowIndex - 1];
-                const nextRow = board[rowIndex + 1];
-                if (previousRow) {
-                    addIfDefined(neighbours, previousRow[columnIndex - 1]);
-                    addIfDefined(neighbours, previousRow[columnIndex]);
-                    addIfDefined(neighbours, previousRow[columnIndex + 1]);
-                }
-
-                addIfDefined(neighbours, board[rowIndex][columnIndex - 1]);
-                addIfDefined(neighbours, board[rowIndex][columnIndex + 1]);
-                if (nextRow) {
-                    addIfDefined(neighbours, nextRow[columnIndex - 1]);
-                    addIfDefined(neighbours, nextRow[columnIndex]);
-                    addIfDefined(neighbours, nextRow[columnIndex + 1]);
-                }
-
-                cell.nextState = getNewState(cell, neighbours);
+        /**
+         * @private
+         * @param {Cell} cell 
+         * @param {Cell[]} neighbours 
+         */
+        getNewState(cell, neighbours) {
+            let numAliveNeighbours = neighbours.filter(n => n.isAlive).length;
+            if (cell.isAlive) {
+                return numAliveNeighbours == 2 || numAliveNeighbours == 3;
+            } else {
+                return numAliveNeighbours == 3;
             }
         }
 
-        for (const row of board) {
-            for (const cell of row) {
-                cell.isAlive = cell.nextState;
-                cell.nextState = null;
+        evaluateBoard() {
+            for (let rowIndex = 0; rowIndex < this.board.length; rowIndex++) {
+                for (let columnIndex = 0; columnIndex < this.board[rowIndex].length; columnIndex++) {
+                    const neighbours = [];
+                    const cell = this.board[rowIndex][columnIndex];
+                    const previousRow = this.board[rowIndex - 1];
+                    const nextRow = this.board[rowIndex + 1];
+                    if (previousRow) {
+                        addIfDefined(neighbours, previousRow[columnIndex - 1]);
+                        addIfDefined(neighbours, previousRow[columnIndex]);
+                        addIfDefined(neighbours, previousRow[columnIndex + 1]);
+                    }
+
+                    addIfDefined(neighbours, this.board[rowIndex][columnIndex - 1]);
+                    addIfDefined(neighbours, this.board[rowIndex][columnIndex + 1]);
+                    if (nextRow) {
+                        addIfDefined(neighbours, nextRow[columnIndex - 1]);
+                        addIfDefined(neighbours, nextRow[columnIndex]);
+                        addIfDefined(neighbours, nextRow[columnIndex + 1]);
+                    }
+
+                    cell.nextState = this.getNewState(cell, neighbours);
+                }
             }
+
+            for (const row of this.board) {
+                for (const cell of row) {
+                    cell.isAlive = cell.nextState;
+                    cell.nextState = null;
+                }
+            }
+        }
+
+        resetBoard() {
+            for (const row of this.board) {
+                for (const cell of row) {
+                    cell.isAlive = false;
+                    cell.nextState = null;
+                }
+            }
+
+            drawBoard(this.board);
         }
     }
 
@@ -98,15 +106,6 @@
         }
 
         return board;
-    }
-
-    /**
-     * @param {number} numRows 
-     * @param {number} numColumns 
-     * @returns {Game}
-     */
-    function createGame(numRows, numColumns) {
-        return new Game(createBoard(numRows, numColumns));
     }
 
     /**
@@ -136,25 +135,11 @@
     }
 
     /**
-     * @param {Cell[][]} board 
-     */
-    function resetBoard(board) {
-        for (const row of board) {
-            for (const cell of row) {
-                cell.isAlive = false;
-                cell.nextState = null;
-            }
-        }
-
-        drawBoard(board);
-    }
-
-    /**
      * @param {Game} game 
      */
     function setupClickListeners(game) {
         document.getElementById('btn-reset').onclick = function() {
-            resetBoard(game.board);
+            game.resetBoard();
         }
 
         document.getElementById('btn-pause').onclick = function() {
@@ -169,12 +154,12 @@
      * @param {number} refreshMs 
      */
     function playGame(numRows, numColumns, refreshMs) {
-        const game = createGame(numRows, numColumns);
+        const game = new Game(createBoard(numRows, numColumns));
         setupClickListeners(game);
         drawBoard(game.board);
         setInterval(function() {
             if (!game.isPaused) {
-                evaluateBoard(game.board);
+                game.evaluateBoard();
                 drawBoard(game.board);
             }
         }, refreshMs);
